d288ebda98eb01523ff6d0a2f67b436ad8adc444
darvesh soni <darveshsoni1@gmail.com>
2025-06-19 17:37:43 +0530

    feat: add description paragraph to LoginPage component

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,6 +4,7 @@ const LoginPage = () => {
   return (
     <div>
       <h1>Login Page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio sed, voluptatem error, incidunt vitae soluta inventore rem placeat neque molestias saepe. Sapiente, similique? Explicabo reprehenderit fuga sunt, culpa a facilis.</p>
     </div>
   )
 }
