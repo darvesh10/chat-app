054e8b4514d8a26777b130b020124be58ce8d03d
darvesh soni <darveshsoni1@gmail.com>
2025-06-20 17:55:04 +0530

    fix: correct typo in homepage description paragraph

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div className="border w-full h-screen ">
       <h1>homepage</h1>
-      <p>this is homepage</p>
+      <p>this is homepage Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi laudantium ex optio quisquam nulla officia quas totam hic eius vero ea pariatur corporis voluptatum voluptate libero illo temporibus, praesentium dolore?</p>
     </div>
   )
 }
