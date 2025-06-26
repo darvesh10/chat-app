1529505b8d48a11d441944b50ae47c51f79df560
darvesh soni <darveshsoni1@gmail.com>
2025-06-26 19:30:03 +0530

    fix: remove unused import from server.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,31 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../config/utils.js";
+
+
+export const signup = async (req, res) => {
+    const { fullName, email, password, bio } = req.body;
+    
+    try {
+        if(!fullName || !email || !password || !bio)  {
+            return res.status(400).json({ error: "All fields are required" });
+          }
+          const user = await User.findOne({email});
+          if(user){
+            return res.status(400).json({ error: "User already exists" });
+          }
+          const salt = await bcrypt.genSalt(10);
+          const hashedPassword = await bcrypt.hash(password, salt);
+          const newUser = await User.create({
+            fullName,
+            email,
+            password: hashedPassword,
+            bio,
+          });
+          const token = generateToken(newUser._id);
+        
+    } catch (error) {
+        
+    }
+}
+
--- server/server.js
+++ server/server.js
@@ -2,14 +2,13 @@ import express from 'express';
 import "dotenv/config";
 import http from 'http';
 import cors from 'cors';
-import { connect } from 'http2';
 import { connectDB } from './config/db.js';
 
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.json({limit: '4mb'}));
-app.use(cors());
+app.use(cors()); 
 
 app.use('/api/status',(req,res)=> res.send({status: 'server is running'}));
 
