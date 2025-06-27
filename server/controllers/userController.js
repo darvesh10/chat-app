63d9d043338ef0b163f77edf486e5b7a5bd99acd
darvesh soni <darveshsoni1@gmail.com>
2025-06-27 19:50:34 +0530

    feat: implement user login, authentication check, and profile update functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../config/utils.js";
+import cloudinary from "../config/cloudinary.js";
 
 
 export const signup = async (req, res) => {
@@ -31,3 +32,52 @@ export const signup = async (req, res) => {
     }
 }
 
+export const login = async (req, res) => {
+  try{
+    const [ email, password ] = req.body;
+    const userData = await User.findOne({ email });
+    const ispasswordValid = await bcrypt.compare(password, userData.password);
+    if( !ispasswordValid){
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+    const token = generateToken(userData._id);
+    res.json({ success: true, userData, token, message: "Login successful" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  };
+}
+
+export const checkAuth =(req,res)=>{
+  res.json({ success: true, user: req.user });
+}
+
+
+export const updateProfile = async (req, res) => {
+  
+  try {
+    const { fullName, profilePic, bio } = req.body;
+    const userId = req.user._id;
+    let updatedUser;
+   
+     if(!profilePic){
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { fullName, bio },
+        { new: true }
+      );
+     } else {
+      const upload = await cloudinary.uploader.upload(profilePic);
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { fullName, profilePic: upload.secure_url, bio },
+        { new: true }
+      );
+     }
+   
+    res.json({ success: true, userData: updatedUser, message: "Profile updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import http from 'http';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const server = http.createServer(app);
@@ -11,10 +12,13 @@ app.use(express.json({limit: '4mb'}));
 app.use(cors()); 
 
 app.use('/api/status',(req,res)=> res.send({status: 'server is running'}));
+app.use('/api/auth', userRouter);
 
 await connectDB();   
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+}
+);
+ 
